9a643d398a22d2eb2318e8165549ab9824fc542e
Parth Singh <parthsinghchauhan1234@gmail.com>
2026-07-28 18:07:42 +0530

    Add support for single CLOUDINARY_URL environment variable configuration

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -21,7 +21,11 @@ try {
 
 let isCloudinaryConfigured = false;
 
-if (process.env.CLOUDINARY_CLOUD_NAME && process.env.CLOUDINARY_API_KEY && process.env.CLOUDINARY_API_SECRET) {
+if (process.env.CLOUDINARY_URL) {
+  cloudinary.config();
+  isCloudinaryConfigured = true;
+  console.log('Cloudinary storage successfully initialized via CLOUDINARY_URL.');
+} else if (process.env.CLOUDINARY_CLOUD_NAME && process.env.CLOUDINARY_API_KEY && process.env.CLOUDINARY_API_SECRET) {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
